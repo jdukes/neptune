1ea1fc8c8ac36d01e616dff8d66935e92cc9ddf2
Josh Dukes <hex@neg9.org>
2015-01-18 07:12:19 -0800

    inital playing around with stuff

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # neptune
 A C and C++ navigator using clang
+
+# requirements
+
+pymake
+
+git@github.com:mozilla/pymake.git
--- neptune/__init__.py
+++ neptune/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+from md5 import md5
 import clang.cindex
 
 class CProjectException(Exception):
@@ -13,22 +15,55 @@ class CProjectException(Exception):
 
 class CProject:
 
-    def __init__(self, path, args=None, max_error_severity=4):
-        """Initialize a CProject
-
-        Takes in path to entrypoint and optional args for compiling.
+    def __init__(self, db_path, load=True)
+        """Initialize a CProject. Constructs a DB
+        
         """
-        self.max_error_severity = max_error_severity
+        self.db_path = db_path)
+        self.tus = []
+        if os.path.exists(self.db_path):
+            self.load_tus()
+        elif load:
+            os.path.mkdir(self.db_path)
         self.index = clang.cindex.Index.create()
+
+    @property
+    def files(self):
+        return (tu.spelling for tu in tus)
+
+    def create_tu_from_file(self, path=None, args=None, max_error_severity=4):
+        self.max_error_severity = max_error_severity
         self.path = path
-        self.tu = self.index.parse(path, args)
+        tu = self.index.parse(path, args)
         if self.errors:
-            raise CProjectException(self.tu.diagnostics)
+            raise CProjectException(tu.diagnostics)
+        self.tus.append(tu)
+
+    def create_tu_from_ast(self, path):
+        tu = self.index.read(path)
+        self.tus.append(tu)
+
+    def load_tus(self):
+        for f in os.listdir(self.path):
+            path = os.path.join(self.path, f)
+            self.create_tu_from_ast(path)
+
+    def save_tus(self):
+        for tu in tus:
+            filename = md5(tu.spelling).hexdigest()
+            tu.save(os.path.join(self.path, filename))
         
     @property
     def errors(self):
         return any(i for i in self.tu.diagnostics
                    if i.severity >= self.max_error_severity)
+
+    def get_xrefs_for(self, name):
+        pass
+        # for i in self.tu.get_includes():
+        #     pass
+        #get_tokens
+        #c.walk_preorder
 
     #can't use emcas column numbers, they are wrong
     def get_coderef_from(self, line, column, path=None):
@@ -93,10 +128,29 @@ class CodeReferece:
     @property
     def refdef(self):
         try:
-            return CodeReferece(self.cursor.referenced, self)
+            return CodeReferece(self.cursor.get_definition(), self)
         except: #fix this 
             return None
 
+    @property
+    def name(self):
+        return self.cursor.spelling #is this always right?
 
+    def get_xrefs_for(self, name):
+        self.parent.get_xrefs_for(name)
+    
+    @property
+    def xrefs(self):
+        self.get_xrefs_for(self.name)
+
+
+def init_db():
+    from sys import argv
+    path = os.path.expanduser(os.path.expandvars(sys.argv.pop()))
+    c = CProject(os.env.get('NEPTUNE_DB_PATH'))
+    c.create_tu_from_file(path, argv)
+    c.save_tus()
+
+    
 # p = CProject(os.path.expanduser('~/src/stegdetect/src/stegdetect-0.6/stegdetect.c'), ' -DHAVE_CONFIG_H -I. -I. -I. -I./jpeg-6b -I./file -I./compat  -I/usr/include/gtk-1.2 -I/usr/include/glib-1.2 -I/usr/lib/glib/include -I/usr/lib/clang/3.5.0/include/ -O2 -Wall -g'.split())
 # c = p.get_coderef_from(1451, 36)
